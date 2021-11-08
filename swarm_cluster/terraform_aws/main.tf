a75b2603f91431a83f560a257c77bd781d8e3327
Alvaro del Castillo San Felix <alvaro.delcastillo@smartprotection.com>
2021-11-08 09:01:18 +0100

    [tf-swarm] Open ssh connection and use modules

--- swarm_cluster/terraform_aws/main.tf
+++ swarm_cluster/terraform_aws/main.tf
@@ -2,4 +2,30 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_instance" "swarm_node" {}
+module "ssh-key" {
+  source = "./modules/ssh-key"
+  key_name = var.key_name
+}
+
+resource "aws_security_group" "ssh" {
+  name = "allow_ssh"
+  description = "Allow SSH connections"
+  ingress {
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "swarm_node" {
+  # Ubuntu Server 20.04 LTS (HVM), SSD Volume Type
+  ami = "ami-08edbb0e85d6a0a07"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.ssh.id]
+  key_name = var.key_name
+
+  tags = {
+    Name = "swarm"
+  }
+}
